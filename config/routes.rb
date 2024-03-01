90669168b9c2249e00a67ae30481efec485b40a0
shareena fatima <shareenafatima40@gmail.com>
2024-03-01 19:46:59 +0500

    added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :posts
   get 'pages/home', to: 'pages#home'
   get 'pages/about', to: 'pages#about'
+  # delete '/users/sign_out', to: 'devise/sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
